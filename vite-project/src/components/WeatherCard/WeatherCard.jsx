a6f720ec318f358adb24b0e54b0ea6c262c6d811
Keri Kiene <mylifeismychild@yahoo.com>
2025-06-05 22:31:09 -0600

    updates

--- vite-project/src/components/WeatherCard/WeatherCard.jsx
+++ vite-project/src/components/WeatherCard/WeatherCard.jsx
@@ -3,15 +3,15 @@ import { weatherOptions } from "../../utils/constants";
 
 function WeatherCard({ weatherData }) {
   const weatherOption = weatherOptions.find((option) => {
-    return option.condition === weatherData.condition;
+    return (
+      option.condition === weatherData.condition &&
+      option.day === weatherData.isDay
+    );
   });
-  console.log("weatherData.condition:", weatherData.condition);
 
   const weatherOptionUrl = weatherOption?.url;
   const weatherOptionCondition = weatherOption?.condition;
 
-  console.log("weatherOption:", weatherOption);
-  console.log("weatherOptionUrl:", weatherOptionUrl);
   return (
     <section className="weather-card">
       <p className="weather-card__temp">{weatherData.temp.F} &deg; F</p>
