dc815f95e52917aa2c602032863f53f50fffe3ca
Keri Kiene <mylifeismychild@yahoo.com>
2025-06-09 15:32:40 -0600

    Updates

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -49,7 +49,7 @@ function App() {
       <ModalWithForm
         title="New garment"
         buttonText="Add garment"
-        activeModal={activeModal}
+        isOpen={activeModal === `add-garment`}
         onClose={closeActiveModal}
       >
         <label htmlFor="name" className="modal__lable">
@@ -73,19 +73,37 @@ function App() {
         <fieldset className="modal__radio-buttons">
           <legend className="modal__legend">Select the weather type:</legend>
           <label htmlFor="hot" className="modal__lable modal__lable_type_radio">
-            <input id="hot" type="radio" className="modal__radio-input" /> Hot
+            <input
+              name="radio"
+              id="hot"
+              type="radio"
+              className="modal__radio-input"
+            />{" "}
+            Hot
           </label>
           <label
             htmlFor="warm"
             className="modal__lable modal__lable_type_radio"
           >
-            <input id="warm" type="radio" className="modal__radio-input" /> Warm
+            <input
+              name="radio"
+              id="warm"
+              type="radio"
+              className="modal__radio-input"
+            />{" "}
+            Warm
           </label>
           <label
             htmlFor="cold"
             className="modal__lable modal__lable_type_radio"
           >
-            <input id="cold" type="radio" className="modal__radio-input" /> Cold
+            <input
+              name="radio"
+              id="cold"
+              type="radio"
+              className="modal__radio-input"
+            />{" "}
+            Cold
           </label>
         </fieldset>
       </ModalWithForm>
--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -11,7 +11,7 @@ function ItemModal({ activeModal, onClose, card }) {
             className="modal__close-btn"
           />
         </button>
-        <img src={card.link} alt="" className="modal__inage" />
+        <img src={card.link} alt="card" className="modal__image" />
         <div className="modal__footer">
           <h2 className="modal__caption">{card.name}</h2>
           <p className="modal__weather">Weather: {card.weather}</p>
